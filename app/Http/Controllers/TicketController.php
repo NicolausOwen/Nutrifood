15d048bdb3614a9e278eaedcda12a96f7f000280
Naufal <naufalbhurr@gmail.com>
2025-04-29 23:17:14 +0700

    feat: make view ticket controller

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class TicketController extends Controller
 {
@@ -18,12 +17,6 @@ class TicketController extends Controller
         return view('tickets.index', compact('tickets'));
     }
 
-    public function indexapi()
-    {
-        $tickets = Ticket::all();
-        return response()->json(['tickets' => $tickets]);
-    }
-
     public function create()
     {
         return view('tickets.create');
@@ -31,29 +24,7 @@ class TicketController extends Controller
 
     public function store(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'id_user' => 'required|exists:users,id',
-        ]);
 
-        if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()], 422);
-        }
-
-        $orderid = "Ticket-" . substr(str_shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), -5);
-        $qrCodeString = Str::uuid()->toString();
-
-        $ticket = Ticket::create([
-            'id' => $orderid,
-            'id_user' => $request->id_user,
-            'checkup' => false,
-            'makan' => false,
-            'used' => false,
-            'qr_code' => $qrCodeString,
-        ]);
-
-        app(EmailController::class)->sendEmail($ticket);
-
-        return response()->json(['tickets' => $ticket, 'qr' => 'https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=' . $qrCodeString]);
     }
 
     public function show($id)
@@ -70,72 +41,12 @@ class TicketController extends Controller
 
     public function update(Request $request, $id)
     {
-        $validator = Validator::make($request->all(), [
-            'id_user' => 'required|exists:users,id',
-            'checkup' => 'required|boolean',
-            'makan' => 'required|boolean',
-            'used' => 'required|boolean',
-        ]);
 
-        if ($validator->fails()) {
-            return redirect()->back()
-                ->withErrors($validator)
-                ->withInput();
-        }
-
-        $ticket = Ticket::findOrFail($id);
-        $ticket->update($request->all());
-
-        return redirect()->route('tickets.show', $ticket->id)
-            ->with('success', 'Ticket updated successfully');
     }
 
     public function destroy($id)
     {
-        $ticket = Ticket::findOrFail($id);
-        $ticket->delete();
 
-        return redirect()->route('tickets.index')
-            ->with('success', 'Ticket deleted successfully');
-    }
-
-    public function validateTicket(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'qr_code' => 'required|string',
-            'target' => 'required|string'
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Invalid QR code'
-            ], 400);
-        }
-
-        $ticket = Ticket::where('qr_code', $request->qr_code)->first();
-
-        if (!$ticket) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Ticket not found'
-            ], 404);
-        }
-
-        $target = $request->target;
-        if ($ticket->$target) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Ticket already used'
-            ], 400);
-        }
-        $ticket->update([$target => true]);
-
-        return response()->json([
-            'success' => true,
-            'message' => 'Ticket validated successfully',
-            'ticket' => $ticket
-        ]);
     }
 
     public function myTickets()
