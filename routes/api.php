79772a5c4e7a9ac887aa8cc4ed6e5b22c315927c
Naufal <naufalbhurr@gmail.com>
2025-04-29 22:46:29 +0700

    refactor: change class api controller

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,15 @@
 <?php
 
-use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TicketController;
 
 // API Ticket routes
 Route::prefix('tickets')->name('api.tickets.')->group(function () {
-    Route::get('/', [TicketController::class, 'indexapi'])->name('indexapi');
+    Route::get('/', [TicketController::class, 'index'])->name('index');
     Route::post('/', [TicketController::class, 'store'])->name('store');
     Route::get('/{id}', [TicketController::class, 'show'])->name('show');
     Route::put('/{id}', [TicketController::class, 'update'])->name('update');
     Route::delete('/{id}', [TicketController::class, 'destroy'])->name('destroy');
-
     Route::post('/validate', [TicketController::class, 'validateTicket'])->name('validate');
     Route::get('/user/my-tickets', [TicketController::class, 'myTickets'])->name('my-tickets');
 });
