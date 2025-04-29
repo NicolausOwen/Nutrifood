4df724b8ebeb5cd4062c95ffa30a578ca4dfc018
Naufal <naufalbhurr@gmail.com>
2025-04-29 22:44:36 +0700

    fix: route conflict with breeze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TicketController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +19,14 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::prefix('tickets')->name('tickets.')->group(function () {
+    Route::get('/', [TicketController::class, 'index'])->name('index');
+    Route::get('/create', [TicketController::class, 'create'])->name('create');
+    Route::get('/{id}', [TicketController::class, 'show'])->name('show');
+    Route::post('/tickets', [TicketController::class, 'store'])->name('store');
+    Route::put('/tickets/{id}', [TicketController::class, 'update'])->name('update');
+    Route::get('/{id}/edit', [TicketController::class, 'edit'])->name('edit');
+    Route::delete('/{id}', [TicketController::class, 'destroy'])->name('destroy');
+    Route::get('/my', [TicketController::class, 'myTickets'])->name('my-tickets');
+});
