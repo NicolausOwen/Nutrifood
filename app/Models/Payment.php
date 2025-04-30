2a8b023db36994e1c42fde9f37094f5416c203e7
PurpsCorps <purpscorps@gmail.com>
2025-04-30 20:32:17 +0700

    feat: make payment API

---
+++ app/Models/Payment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'id',
+        'ticket_id',
+        'payment_proof',
+        'created_at',
+        'verification_payment',
+        'verification_at',
+    ];
+}
