e44c448595bf39fbb03b903e4dff0aa75791ef9b
PurpsCorps <purpscorps@gmail.com>
2025-06-07 08:27:19 +0700

    perf: race pack on dashboard ticket

--- resources/views/user/dashboard-ticket.blade.php
+++ resources/views/user/dashboard-ticket.blade.php
@@ -322,13 +322,13 @@
                                     <p class="${masukStatus}">Check In</p>
                                 </div>
                                 <div class="text-center">
-                                    <div class="w-8 h-8 rounded-full mx-auto mb-1 flex items-center justify-center ${makanBg}">
-                                        ${makanUsed ?
+                                    <div class="w-8 h-8 rounded-full mx-auto mb-1 flex items-center justify-center ${checkupBg}">
+                                        ${checkupUsed ?
                                             '<svg class="w-4 h-4 text-green-600" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>'
                                             : '<svg class="w-4 h-4 text-gray-400" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>'
                                         }
                                     </div>
-                                    <p class="${makanStatus}">Makan</p>
+                                    <p class="${checkupStatus}">Race-Pack</p>
                                 </div>
                             </div>
                         </div>
